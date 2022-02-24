a7bc52fa1a449877ba5513f0f3251b3854550eaf
Marcos Rigel <marcosrigel99@gmail.com>
2022-02-23 23:48:37 -0300

    Praticando ocm Java Puro: Singleton

--- lab-padroes-projeto-java/src/one/digitalinnovation/gof/SingletonLazy.java
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/SingletonLazy.java
@@ -1,5 +1,18 @@
 package one.digitalinnovation.gof;
 
+/* Singleton "preguiçoso" */
 public class SingletonLazy {
 
+	private static SingletonLazy instancia;
+	
+	public SingletonLazy() {
+		super();
+	}
+	
+	public static SingletonLazy getInstancia() {
+		if (instancia == null) {
+			instancia = new SingletonLazy();
+		}
+		return instancia;
+	}
 }
---
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/SingletonLazyHolder.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof;
+
+/* Singleton "Lazy Holder" 
+ * 
+ * https://stackoverflow.com/a/24018148
+ * 
+ * */
+public class SingletonLazyHolder {
+
+	private static class InstanceHolder {
+		public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+	}
+	public SingletonLazyHolder() {
+		super();
+	}
+	
+	public static SingletonLazyHolder getInstancia() {
+		return InstanceHolder.instancia;
+	}
+}
